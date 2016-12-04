293237e5008fb53aed3d3272a1e9b208b020e75c
Atanda <abdulsemiu.atanda@andela.com>
2016-12-05 00:17:53 +0100

    Filtering parameters fix in publication

--- imports/plugins/included/search-mongo/server/publications/searchresults.js
+++ imports/plugins/included/search-mongo/server/publications/searchresults.js
@@ -2,18 +2,80 @@ import _ from "lodash";
 import { Meteor } from "meteor/meteor";
 import { check, Match } from "meteor/check";
 import { Reaction, Logger } from "/server/api";
+import { EJSON } from "meteor/ejson";
 import { ProductSearch, OrderSearch, AccountSearch } from "/lib/collections";
 
 const supportedCollections = ["products", "orders", "accounts"];
+function checkPriceRange(priceRange, shopId, searchTerm) {
+  let findTerm = {};
+  switch (priceRange) {
+    case "below-10":
+      findTerm = {
+        "price.min": { $gt: 0.00 },
+        "price.max": { $lt: 10.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    case "10-55":
+      findTerm = {
+        "price.min": { $gt: 10.00 },
+        "price.max": { $lt: 55.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    case "55-100":
+      findTerm = {
+        "price.min": { $gt: 55.00 },
+        "price.max": { $lt: 100.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    case "100-500":
+      findTerm = {
+        "price.min": { $gt: 100.00 },
+        "price.max": { $lt: 500.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    case "500-1000":
+      findTerm = {
+        "price.min": { $gt: 500.00 },
+        "price.max": { $lt: 1000.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    case "1000-above":
+      findTerm = {
+        "price.min": { $gt: 1000.00 },
+        "price.max": { $lt: 10000000.00 },
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+      break;
+    default:
+      findTerm = {
+        shopId: shopId,
+        $text: { $search: searchTerm }
+      };
+  }
 
-function getProductFindTerm(searchTerm, searchTags, userId) {
+  return findTerm;
+}
+function getProductFindTerm(searchTerm, searchTags, priceRange, brandPicked, userId) {
   const shopId = Reaction.getShopId();
-  const findTerm = {
-    shopId: shopId,
-    $text: {$search: searchTerm}
-  };
-  if (searchTags.length) {
+  const findTerm = checkPriceRange(priceRange, shopId, searchTerm);
+  if (searchTags.length > 0) {
     findTerm.hashtags = {$all: searchTags};
+  }
+  if (typeof brandPicked === "string") {
+    if (brandPicked.length > 1) {
+      findTerm.brand = brandPicked;
+    }
   }
   if (!Roles.userIsInRole(userId, ["admin", "owner"], shopId)) {
     findTerm.isVisible = true;
@@ -23,9 +85,11 @@ function getProductFindTerm(searchTerm, searchTags, userId) {
 
 export const getResults = {};
 
-getResults.products = function (searchTerm, facets, maxResults, userId) {
+getResults.products = function (searchTerm, facets, maxResults, userId, priceRange,
+  brandPicked) {
   const searchTags = facets || [];
-  const findTerm = getProductFindTerm(searchTerm, searchTags, userId);
+  const findTerm = getProductFindTerm(searchTerm, searchTags, priceRange,
+    brandPicked, userId);
   const productResults = ProductSearch.find(findTerm,
     {
       fields: {
@@ -34,7 +98,9 @@ getResults.products = function (searchTerm, facets, maxResults, userId) {
         hashtags: 1,
         description: 1,
         handle: 1,
-        price: 1
+        price: 1,
+        brand: 1,
+        numSold: 1
       },
       sort: {score: {$meta: "textScore"}},
       limit: maxResults
@@ -116,16 +182,22 @@ getResults.accounts = function (searchTerm, facets, maxResults, userId) {
   return accountResults;
 };
 
-Meteor.publish("SearchResults", function (collection, searchTerm, facets, maxResults = 99) {
+Meteor.publish("SearchResults", function (collection, searchTerm, facets,
+  priceRange = "", brandPicked = "", maxResults = 99) {
   check(collection, String);
   check(collection, Match.Where((coll) => {
     return _.includes(supportedCollections, coll);
   }));
+  check(brandPicked, Match.OneOf(String, undefined, null));
+  check(priceRange, Match.OneOf(String, undefined, null));
   check(searchTerm, Match.Optional(String));
   check(facets, Match.OneOf(Array, undefined));
   Logger.debug(`Returning search results on ${collection}. SearchTerm: |${searchTerm}|. Facets: |${facets}|.`);
+  if (brandPicked === "&all&" || brandPicked === "&null&") {
+    brandPicked = null;
+  }
   if (!searchTerm) {
     return this.ready();
   }
-  return getResults[collection](searchTerm, facets, maxResults, this.userId);
+  return getResults[collection](searchTerm, facets, maxResults, this.userId, priceRange, brandPicked);
 });
