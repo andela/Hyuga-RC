e17fcb10fad50c55d4ba9dcc80d526a0a64eb7e4
Femi Oladeji <femidotexe@gmail.com>
2016-12-16 14:43:08 +0100

    Feature(wallet): Handle display of wallet records

--- imports/plugins/core/layout/client/templates/layout/wallet/wallet.js
+++ imports/plugins/core/layout/client/templates/layout/wallet/wallet.js
@@ -2,11 +2,11 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 
 Template.wallet.events({
-  "click #deposit": (event) => {
+  "submit #deposit": (event) => {
     event.preventDefault();
     const amount = parseInt(document.getElementById("depositAmount").value, 10);
-    const transaction = {amount: amount, referenceId: "d8s8fjj983r83ejppu8d88ru34", date: new Date()};
-    Meteor.call("wallet/transaction", Meteor.userId(), transaction, "deposit", (err, res) => {
+    const transaction = {amount: amount, referenceId: "d8s8fjj983r83ejppu8d88ru34", date: new Date(), transactionType: "Debit"};
+    Meteor.call("wallet/transaction", Meteor.userId(), transaction, (err, res) => {
       if (res) {
         document.getElementById("depositAmount").value = "";
         Alerts.toast("Your deposit was successful", "success");
@@ -20,15 +20,48 @@ Template.wallet.events({
     event.preventDefault();
     const amount = parseInt(document.getElementById("transferAmount").value, 10);
     const to = document.getElementById("recipient").value;
-    const transaction = {amount, to, date: new Date()};
-    Meteor.call("wallet/transaction", Meteor.userId(), transaction, "payment", (err, res) => {
+    const transaction = {amount, to, date: new Date(), transactionType: "Credit"};
+    Meteor.call("wallet/transaction", Meteor.userId(), transaction, (err, res) => {
       if (res === 2) {
         Alerts.toast(`No user with email ${to}`, "error");
       } else if (res === 1) {
+        document.getElementById("recipient").value = "";
+        document.getElementById("transferAmount").value = "";
         Alerts.toast("The transfer was successful", "success");
       } else {
         Alerts.toast("An error occured, please try again", "error");
       }
     });
+  }
+});
+
+Template.wallet.onCreated(function () {
+  this.state = new ReactiveDict();
+  this.autorun(() => {
+    this.state.setDefault({
+      balance: 0,
+      transactions: []
+    });
+    const instance = this;
+    Meteor.call("wallet/getTransaction", Meteor.userId(), (err, res) => {
+      if (res) {
+        instance.state.set("balance", res.balance);
+        instance.state.set("transactions", res.transactions);
+      }
+    });
+  });
+});
+
+Template.wallet.helpers({
+  balance: () => {
+    return Template.instance().state.get("balance");
+  },
+
+  formatCurrency: (number) => {
+    return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+  },
+
+  getTransactions: () => {
+    return Template.instance().state.get("transactions");
   }
 });
--- server/methods/core/wallet.js
+++ server/methods/core/wallet.js
@@ -12,42 +12,34 @@ Meteor.methods({
    * @param {string} type, type of transaction done
    * @return {boolean} true or false if the db operation was successful
    */
-  "wallet/transaction": (userId, transaction, type) => {
+  "wallet/transaction": (userId, transactions) => {
     check(userId, String);
-    check(type, String);
-    let pushOptions, balanceOptions;
-    const {amount, date} = transaction;
-    const basicInfo = {amount, date};
-    if (type === "deposit") {
-      check(transaction, Schemas.Deposits);
-      if (transaction.referenceId) {
-        basicInfo.referenceId = transaction.referenceId;
-      }
-      if (transaction.from) {
-        basicInfo.from = transaction.from;
-      }
-      pushOptions = {deposits: basicInfo};
+    check(transactions, Schemas.Transaction);
+    let balanceOptions;
+    const {amount, transactionType} = transactions;
+    if (transactionType === "Debit") {
       balanceOptions = {balance: amount};
     }
-    if (type === "payment") {
-      check(transaction, Schemas.Withdrawals);
-      basicInfo.orderId = transaction.orderId;
-      if (transaction.to) {
-        const recipient = Collections.Accounts.findOne({"emails.0.address": transaction.to});
+    if (transactionType === "Credit") {
+      if (transactions.to) {
+        const recipient = Collections.Accounts.findOne({"emails.0.address": transactions.to});
         const sender = Collections.Accounts.findOne(userId);
         if (!recipient) {
           return 2;
         }
         // deposit for the recipient
-        Meteor.call("wallet/transaction", recipient._id, {amount, from: sender.emails[0].address, date: new Date()}, "deposit");
-        basicInfo.to = transaction.to;
+        Meteor.call("wallet/transaction", recipient._id, {
+          amount,
+          from: sender.emails[0].address,
+          date: new Date(),
+          transactionType: "Debit"
+        });
       }
-      pushOptions = {withdrawals: basicInfo};
       balanceOptions = {balance: -amount};
     }
 
     try {
-      Collections.Wallet.update({userId}, {$push: pushOptions, $inc: balanceOptions}, {upsert: true});
+      Collections.Wallet.update({userId}, {$push: {transactions: transactions}, $inc: balanceOptions}, {upsert: true});
       return 1;
     } catch (error) {
       return 0;
@@ -62,6 +54,11 @@ Meteor.methods({
    * @return {boolean} true or false if the db operation was successful
    */
   "wallet/getTransaction": (userId) => {
-    return Wallet.findOne({userId});
+    check(userId, String);
+    return Collections.Wallet.findOne({userId});
   }
 });
+
+// Meteor.publish("transactionDetails", () => {
+//   return Collections.findOne({userId: this.userId});
+// });
