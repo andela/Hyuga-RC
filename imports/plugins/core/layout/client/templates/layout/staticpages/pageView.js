c5647bdd0563c93e81837feabb77cc3f4b2b8229
andela-oagunbiade <agunbiade.oreoluwa@gmail.com>
2016-12-30 18:45:41 +0100

    update(layout): add staticpages layout

---
+++ imports/plugins/core/layout/client/templates/layout/staticpages/pageView.js
@@ -0,0 +1,19 @@
+import { StaticPages } from "/lib/collections";
+
+Template.pageView.onCreated(() => {
+  this.subscribe("Pages");
+});
+
+Template.pageView.helpers({
+  /**
+   * @param {String} pageRoute - The route of the page that is to be returned
+   * @return {Array} The array of data for the page gotten from the database
+   */
+  singlePage(pageRoute) {
+    const page = StaticPages.findOne({
+      pageRoute: `pages/${pageRoute}`
+    });
+    const pageData = [page];
+    return pageData;
+  }
+});
