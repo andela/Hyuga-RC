059677bb25aa6eee6b1692838416fe86f9ca7ad9
Ethan Nwankwo <emekanwankwo201@gmail.com>
2017-01-14 18:06:56 +0100

    feature(add-pie-chart): add pie chart to analytics page.

--- imports/plugins/core/layout/client/templates/layout/analytics/analytics.js
+++ imports/plugins/core/layout/client/templates/layout/analytics/analytics.js
@@ -2,6 +2,7 @@ import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 
 Template.analyticsPage.helpers({
-   
-
+  userData() {
+    return { newUsers: 2, existingUsers: 4 };
+  }
 });
