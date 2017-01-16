90de1545e8df99dc3376c5e1ccf5a636d9dd3500
Joshua Azemoh <joshazemoh@gmail.com>
2017-01-16 17:58:11 +0100

    feature(api-endpoints): implement partial put action

--- server/startup/rest-endpoints.js
+++ server/startup/rest-endpoints.js
@@ -6,13 +6,35 @@ export default () => {
     prettyJson: true
   });
 
-  const options = {
-    excludedEndpoints: ["delete"]
+  const getOptions = (collection) => {
+    return {
+      excludedEndpoints: ["delete"],
+      endpoints: {
+        put: {
+          action() {
+            const isUpdated = collection.update(this.urlParams.id, {
+              $set: this.bodyParams
+            });
+
+            if (isUpdated) {
+              const record = collection.findOne(this.urlParams.id);
+
+              return { status: "success", data: record };
+            }
+
+            return {
+              statusCode: 404,
+              body: { status: "fail", message: "Item not found", body: this.bodyParams }
+            };
+          }
+        }
+      }
+    };
   };
 
-  Api.addCollection(Accounts, options);
-  Api.addCollection(Cart, options);
-  Api.addCollection(Orders, options);
-  Api.addCollection(Products, options);
-  Api.addCollection(Shops, options);
+  Api.addCollection(Accounts, getOptions(Accounts));
+  Api.addCollection(Cart, getOptions(Cart));
+  Api.addCollection(Orders, getOptions(Orders));
+  Api.addCollection(Shops, getOptions(Shops));
+  Api.addCollection(Products, getOptions(Products));
 };
